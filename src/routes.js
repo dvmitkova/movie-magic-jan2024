d7e1a3f7cdab9cceaa513131e4ad3c9bf9d12f70
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-01-22 15:21:05 +0200

    Add create movie page

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();//използваме вградената ф-я Router();
 
 const homeController = require('./controllers/homeController');
+const movieController = require('./controllers/movieController');
 
 router.use(homeController);
+router.use(movieController);
+
 
 module.exports = router;
