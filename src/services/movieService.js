169851cdac63d7326b3f09b00a6bf0af9c7cf8c9
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-01-23 10:12:57 +0200

    Add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,14 @@
-const movies = [];
+const movies = [{
+    title: 'Batman',
+    genre: 'Action',
+    director: 'No man',
+    date: '2010',
+    imageUrl: 'https://m.media-amazon.com/images/M/MV5BOTM3MTRkZjQtYjBkMy00YWE1LTkxOTQtNDQyNGY0YjYzNzAzXkEyXkFqcGdeQXVyOTgwMzk1MTA@._V1_FMjpg_UX1000_.jpg',
+    rating: '8.9',
+    description: 'Based on the comics'
+}];
 
 exports.create = (movieData) => {
+    console.log(movieData);
     movies.push(movieData);
 }
