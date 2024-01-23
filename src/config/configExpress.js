fff1a0291990de5e82de29468a3225cde57b8de4
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-01-23 10:08:57 +0200

    Add body parser

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -4,7 +4,9 @@ const path = require('path');
 function configExpress(app) {
 app.use(express.static(path.resolve('src/static')));//CWD = Current Working Directory
 //.resolve се базира на CWD;
-
+app.use(express.urlencoded({ extended: false }));
+//middleware, който чете и парсва информацията в body-to преди данните да дойдат при нас.
+//Използва се за извличане на данни от форма.
 return app;
 }
 
