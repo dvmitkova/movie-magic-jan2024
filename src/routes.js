ae8f185df9799486a3d8eb2734bf0157c1689ab8
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-01-26 15:13:29 +0200

    Add cast page

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();//използваме вграден
 
 const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
+const castController = require('./controllers/castController');
 
 router.use(homeController);
 router.use(movieController);
+router.use('/cast', castController);
 
 router.get('*', (req, res) => {//взимаме всичко, което не е намерено от controllers и го пращаме към page /404;
     res.redirect('/404');
