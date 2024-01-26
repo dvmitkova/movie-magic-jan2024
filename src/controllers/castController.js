7a82e9000cc9d1a75fbd1d48dd5570b233e953e9
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-01-26 15:18:43 +0200

    Add post action for cast-create

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -4,4 +4,12 @@ router.get('/create', (req, res) => {
     res.render('cast/create');
 });
 
+router.post('/create', (req, res) => {//когато правим post заявка на страницата:
+    const body = req.body;//взимаме данните
+
+    console.log(body);//логваме ги
+
+    res.redirect('/');//връщаме на нач. страница;
+})
+
 module.exports = router;
