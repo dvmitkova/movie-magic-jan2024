d9a8bdad69047793a0b205e7ba4cc07919dd2c5b
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-02-01 14:34:00 +0200

    Add register page

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();//използваме вграден
 const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
 const castController = require('./controllers/castController');
+const authController = require('./controllers/authController');
 
 router.use(homeController);
 router.use(movieController);
 router.use('/cast', castController);
+router.use('/auth', authController);
 
 router.get('*', (req, res) => {//взимаме всичко, което не е намерено от controllers и го пращаме към page /404;
     res.redirect('/404');
