4450341bc32b21d2f4a98bbca292d12fabe4260f
Diana Mitkova <diana_guerdjilova@abv.bg>
2024-02-02 15:22:02 +0200

    Move details view

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -34,7 +34,7 @@ router.get('/movies/:movieId', async (req, res) => {
     //TODO This is not perfect, use handlebars helpers
     //movie.ratingStars = '&#x2605;'.repeat(movie.rating)
 
-    res.render('details', { movie });
+    res.render('movie/details', { movie });
 });
 
 router.get('/movies/:movieId/attach', isAuth, async (req, res) => {
